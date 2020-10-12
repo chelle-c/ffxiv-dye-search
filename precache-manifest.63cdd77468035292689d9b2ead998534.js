bb2dc7bb2ffd65a945aa9e0509642301bf2097ee
Chelle <9515162+chelle-c@users.noreply.github.com>
2020-10-11 22:28:48 -0230

    Updates

---
+++ precache-manifest.63cdd77468035292689d9b2ead998534.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bf93a4056432f6c3448f88f49e96ce6",
+    "url": "/ffxiv-dye-search/index.html"
+  },
+  {
+    "revision": "d6a1a051a4df2267b9a9",
+    "url": "/ffxiv-dye-search/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "52498e9c301a5e201f06",
+    "url": "/ffxiv-dye-search/static/js/2.f4fa255c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/ffxiv-dye-search/static/js/2.f4fa255c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6a1a051a4df2267b9a9",
+    "url": "/ffxiv-dye-search/static/js/main.8f1bbb43.chunk.js"
+  },
+  {
+    "revision": "24cc42d390b229a03310",
+    "url": "/ffxiv-dye-search/static/js/runtime-main.5c485951.js"
+  }
+]);
